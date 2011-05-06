bd10e07dd5f3ef305edf631772eeb5639b3b5bbf
Anssi Kostiainen <anssi.kostiainen@gmail.com>
2011-05-06 11:13:22 +0300

    auto-rotate defines for Maemo to fix Symbian build

--- main.cpp
+++ main.cpp
@@ -8,8 +8,12 @@ int main(int argc, char *argv[])
     QmlApplicationViewer viewer;
     //viewer.setOrientation(QmlApplicationViewer::ScreenOrientationLockPortrait);
     viewer.setMainQmlFile(QLatin1String("qml/qml/webbrowser.qml"));
-    // auto-rotate on Maemo5
+
+    #if defined(Q_WS_MAEMO_5) || defined(Q_WS_MAEMO_6)
+    // auto-rotate on Maemo
     viewer.setAttribute(Qt::WA_Maemo5AutoOrientation, true);
+    #endif
+
     viewer.showExpanded();
 
     return app.exec();
